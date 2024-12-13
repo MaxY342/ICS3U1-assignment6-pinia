c5f130ef2c0014415e137f57e1e9cb41b342dbb2
Max Yang <max.yang342@gmail.com>
2024-12-13 15:15:19 -0500

    update cart css

--- netflix-replica/src/views/CartView.vue
+++ netflix-replica/src/views/CartView.vue
@@ -12,10 +12,14 @@ console.log(store);
     <div class="cart">
         <div class="content">
             <h1>Shopping Cart</h1>
-            <div class="item" v-for="([key, value]) in store.cart">
-                <img :src="`https://image.tmdb.org/t/p/w500${value.url}`" />
-                <h1>{{ value.title }}</h1>
-                <button @click="store.cart.delete(key)">Remove</button>
+            <div class="item-container">
+                <div class="item" v-for="([key, value]) in store.cart">
+                    <img :src="`https://image.tmdb.org/t/p/w500${value.url}`" />
+                    <h1>{{ value.title }}</h1>
+                    <div class="right-details">
+                        <button @click="store.cart.delete(key)">Remove</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -28,21 +32,29 @@ console.log(store);
     border-radius: 10px;
     color: white;
 }
+
 h1 {
   text-align: center;
-  margin-bottom: 20px;
   font-size: 2.5rem;
-  margin-bottom: 4rem;
 }
 
 button {
-    display: inline-block;
-    margin-bottom: 20px;
-    padding: 10px 15px;
-    background-color: #e50914;
-    color: white;
-    text-decoration: none;
-    border-radius: 5px;
+  background-color: #e20c0c;
+  color: white;
+  padding: 1rem;
+  border: 0;
+  border-radius: 10px;
+  transition: transform 0.2s;
+}
+
+button:hover {
+  background-color: #c20000;
+  cursor: pointer;
+}
+
+.right-details {
+    display: flex;
+    justify-content: flex-end;
 }
 
 .cart {
@@ -53,14 +65,23 @@ button {
     gap: 2rem;
 }
 
-.item {
+.item-container {
     display: flex;
+    flex-direction: column;
+    gap: 5rem;
+}
+
+.item {
+    display: grid;
+    grid-template-columns: auto auto 1fr auto;
+    gap: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
     align-items: center;
-    gap: 2rem;
 }
 
 img {
-    width: 10%;
+    width: 30%;
     border-radius: 5%;
 }
 </style>
